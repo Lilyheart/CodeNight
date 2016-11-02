4e17b80740c0e519bbb8c84d9fb5bfa5bd5f5802
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-02 18:37:35 -0400

    finalizeing Problem5 and its unittests

---
+++ javascript/problems/Problem5.js
@@ -0,0 +1,35 @@
+/*
+*    Problem 5:
+*
+*    There is always money in the banana stand
+*
+*    NO TOUCHING (https://www.youtube.com/watch?v=UuHILqDIvis)
+*
+*    When you have problems with money, you should always look
+*    in the banana stand, but we have to find it first
+*
+*    Create a function that takes in a string and returns anything
+*    that is between the words banana and stand.
+*
+*    If there is no banana and/or stand, return an string: 'No Money!'
+*/
+
+function GetWhatsInBananaStand(fullText) {
+    /*
+    *    Input: string
+    *    Output: string
+    *
+    *    Examples:
+    *        'banana money stand' -> ' money '
+    *        'I like bananas, where do you stand on the issue?' ->
+    *            's, where do you '
+    *        'banana only' -> ''
+    *        'stand only' -> ''
+    */
+
+    var textInBananaStand = '';
+
+    // Get text between banana and stand
+
+    return textInBananaStand;
+}
