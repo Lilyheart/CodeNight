77dd55a55e2af74a6c96a37eefc4739f7289333d
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-01 23:25:42 -0400

    adding 5th problem and unit tests

--- python/problems/problem_5.py
+++ python/problems/problem_5.py
@@ -2,15 +2,35 @@
 """
     Problem 5:
 
+    There is always money in the banana stand
+
+    NO TOUCHING (https://www.youtube.com/watch?v=UuHILqDIvis)
+
+    When you have problems with money, you should always look
+    in the banana stand, but we have to find it first
+
+    Create a function that takes in a string and returns anything
+    that is between the words banana and stand.
+
+    If there is no banana and/or stand, return an string: 'No Money!'
 """
 
 
-def problem(input):
+def get_whats_in_banana_stand(text):
     """
-        Input:
-        Output:
+        Input: string
+        Output: string
 
         Examples:
+            'banana money stand' -> ' money '
+            'I like bananas, where do you stand on the issue?' ->
+                's, where do you '
+            'banana only' -> ''
+            'stand only' -> ''
     """
 
-    return input
+    text_in_banana_stand = ''
+
+    # Get text between banana and stand
+
+    return text_in_banana_stand
--- python/unittests/test_problem_5.py
+++ python/unittests/test_problem_5.py
@@ -10,13 +10,28 @@ import unittest
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_PATH + '/../')
 
-from problems.problem_5 import problem
+from problems.problem_5 import get_whats_in_banana_stand
 
 
 class TestProblem5(unittest.TestCase):
 
-    def test(self):
-        self.assertEqual(True, True)
+    def test_no_banana(self):
+        self.assertEqual(
+            get_whats_in_banana_stand('stand only'),
+            'No Money!')
+
+    def test_no_stand(self):
+        self.assertEqual(
+            get_whats_in_banana_stand('banana only'),
+            'No Money!')
+
+    def test_inside(self):
+        self.assertEqual(
+            get_whats_in_banana_stand('I like bananas, where do you stand on the issue?'),
+            's, where do you ')
+        self.assertEqual(
+            get_whats_in_banana_stand('banana money stand'),
+            ' money ')
 
 
 if __name__ == "__main__":
