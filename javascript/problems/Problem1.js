0864379718dc6b61ec3b8b35d9cf65fe748948ae
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-02 18:19:07 -0400

    adding javascript directories and Problem1 in javascript

---
+++ javascript/problems/Problem1.js
@@ -0,0 +1,40 @@
+/*
+*    Problem 1:
+*
+*    Christopher Walken is known for putting his own spin
+*    on how to read his lines. Specifically, he likes to
+*    insert pauses, which would be denoted by a period.
+*
+*    Create a function that takes a string, and inserts a
+*    period after each 5th word.
+*
+*    Since a period is being added after each 5th word, the
+*    the following word should be capitalized, to add emphasis
+*
+*    All quotes should end with a period (If a period already
+*    exists, skip to the next 5th word)
+*
+*    If nothing is passed, return 'More Cowbell!'
+*
+*    Great Example: https://vimeo.com/55624839
+*/
+
+function WalkenSimulator(quote) {
+    /*
+    *    Input: string
+    *    Output: string
+    *
+    *    Examples:
+    *
+    *      "" -> "More Cowbell!"
+    *
+    *      "You're talking to me all wrong" ->
+    *          "You're talking to me all. Wrong."
+    */
+
+    var walkenQuote = quote;
+
+    // Walkenize Quote Here
+
+    return walkenQuote;
+}
