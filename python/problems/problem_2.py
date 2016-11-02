02850bfebc69c8ea6f359e822df1f4efe15d8ad8
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-01 22:28:55 -0400

    adding problem 2 and unittests

--- python/problems/problem_2.py
+++ python/problems/problem_2.py
@@ -2,15 +2,50 @@
 """
     Problem 2:
 
+    Sean Bean doesn't always die...
+
+    (But he usually does: https://www.youtube.com/watch?v=zEhtsgu6bJg)
+
+    Return a list of movie titles in which Sean bean does
+    and does not die, sorted from the year that they came out
 """
 
 
-def problem(input):
+def sean_bean_survival_and_deaths():
     """
-        Input:
-        Output:
-
-        Examples:
+        Input: None
+        Output: list, list
     """
 
-    return input
+    sean_bean_movies = [
+        {
+            'movie': 'The Load of the Rings: Fellowship of the Ring',
+            'year': 2001,
+            'dies': True
+        },{
+            'movie': 'Patriot Games',
+            'year': 1992,
+            'dies': True
+        },{
+            'movie': 'Golden Eye',
+            'year': 1995,
+            'dies': True
+        },{
+            'movie': 'Silent Hill',
+            'year': 2006,
+            'dies': False
+        },{
+            'movie': 'National Treasure',
+            'year': 2004,
+            'dies': False
+        },{
+            'movie': 'The Martian',
+            'year': 2015,
+            'dies': False
+        }
+    ]
+
+    survival_movies = []
+    death_movies = []
+
+    return survival_movies, death_movies
--- python/unittests/test_problem_2.py
+++ python/unittests/test_problem_2.py
@@ -10,13 +10,38 @@ import unittest
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_PATH + '/../')
 
-from problems.problem_2 import problem
+from problems.problem_2 import sean_bean_survival_and_deaths
 
 
 class TestProblem2(unittest.TestCase):
 
-    def test(self):
-        self.assertEqual(True, True)
+    def test_survival(self):
+
+        returned_survival_movies, returned_death_movies =\
+            sean_bean_survival_and_deaths()
+
+        expected_survivial_movies = [
+            'National Treasure',
+            'Silent Hill',
+            'The Martian',
+        ]
+
+        self.assertEqual(returned_survival_movies,
+            expected_survivial_movies)
+
+    def test_death(self):
+
+        returned_survival_movies, returned_death_movies =\
+            sean_bean_survival_and_deaths()
+
+        expected_death_movies = [
+            'Patriot Games',
+            'Golden Eye',
+            'The Load of the Rings: Fellowship of the Ring',
+        ]
+
+        self.assertEqual(returned_death_movies,
+            expected_death_movies)
 
 
 if __name__ == "__main__":
