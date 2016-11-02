76990400033353a4572890ce4931cbb3711bf36f
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-01 23:05:17 -0400

    cerating problem 4 and tests to deal with dates

--- python/problems/problem_4.py
+++ python/problems/problem_4.py
@@ -2,15 +2,35 @@
 """
     Problem 4:
 
+    Timezones are important to consider when developing
+    any application.
+
+    Write a function to take a passed in datetime string
+    from Eastern Time (ET), and return a datetime string
+    with the correct time for Pacific Time (PT)
+
+    Eastern Time is 3 hours ahead of Pacific Time
+
+    Format should be year-month-date hours:minutes:seconds
+    (YYYY-MM-DD HH:MM:SS)
+
+    If an invalid form is passed, return an empty string
 """
 
+# Imports Here
 
-def problem(input):
+
+def timezone_conversion(et_datetime):
     """
-        Input:
-        Output:
+        Input: string (YYYY-MM-DD HH:MM:SS)
+        Output: string (YYYY-MM-DD HH:MM:SS)
 
         Examples:
+            2016-10-31 01:00:00 -> 2016-10-30 22:00:00
     """
 
-    return input
+    pt_datetime = et_datetime
+
+    # Timezone Conversion Here
+
+    return pt_datetime
--- python/unittests/test_problem_4.py
+++ python/unittests/test_problem_4.py
@@ -10,13 +10,28 @@ import unittest
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_PATH + '/../')
 
-from problems.problem_4 import problem
+from problems.problem_4 import timezone_conversion
 
 
 class TestProblem4(unittest.TestCase):
 
-    def test(self):
-        self.assertEqual(True, True)
+    def test_not_a_datetime(self):
+        self.assertEqual(timezone_conversion(''), '')
+        self.assertEqual(
+            timezone_conversion('2016-10-31'), '')
+        self.assertEqual(
+            timezone_conversion('01:00:00'), '')
+
+    def test_conversions(self):
+        self.assertEqual(
+            timezone_conversion('2016-10-31 01:00:00'),
+            '2016-10-30 22:00:00')
+        self.assertEqual(
+            timezone_conversion('2016-10-31 03:00:00'),
+            '2016-10-31 00:00:00')
+        self.assertEqual(
+            timezone_conversion('2017-01-01 20:24:36'),
+            '2017-01-01 23:24:36')
 
 
 if __name__ == "__main__":
