dd52e2c57797ad42c7930120732f3b12640d86e8
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-11-01 22:04:15 -0400

    adding problem 3 and tests

--- python/problems/problem_3.py
+++ python/problems/problem_3.py
@@ -2,15 +2,53 @@
 """
     Problem 3:
 
+    Texting is the most common form of communication,
+    but it used to suck. Before mobile OSs had touch
+    keyboards, we had to use the number keys to type.
+    Each number had some characters associated with them,
+    with the following mapping:
+
+    1 -> .,!?1
+    2 -> abc2
+    3 -> def3
+    4 -> ghi4
+    5 -> jkl5
+    6 -> mno6
+    7 -> pqrs7
+    8 -> tuv8
+    9 -> wxyz9
+    0 -> 0
+    * -> space
+
+    To type lol, the user would have to type 5 3 times in
+    a row, pause, type 6 3 times in a row, pause, then
+    type 5 times in a row.
+
+    Create a function that turns a passed string into
+    the keys that would need to be pressed
+
+    pauses should be displayed as underscores
+
+    If you wanted to enter a number, you would have to pass
+    each letter for the key (4 presses of the number 2
+    to get the number 2 to print)
+
+    Video: https://www.youtube.com/watch?v=3lTIQT0X-3A
 """
 
 
-def problem(input):
+def phone_text_conversion(text):
     """
-        Input:
-        Output:
+        Input: string
+        Output: string
 
         Examples:
+            lol -> 555_666_555
+            k -> 55
+            l8r -> 555_8888_777
+            no way! - 66_666_*_9_2_999_111
     """
 
-    return input
+    key_presses = ''
+
+    return key_presses
--- python/unittests/test_problem_3.py
+++ python/unittests/test_problem_3.py
@@ -10,13 +10,40 @@ import unittest
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_PATH + '/../')
 
-from problems.problem_3 import problem
+from problems.problem_3 import phone_text_conversion
 
 
 class TestProblem3(unittest.TestCase):
 
-    def test(self):
-        self.assertEqual(True, True)
+    def test_nothing(self):
+        self.assertEqual(
+            phone_text_conversion(''), '')
+
+    def test_omg(self):
+        self.assertEqual(
+            phone_text_conversion('omg'), '666_6_4')
+
+    def test_inbd(self):
+        self.assertEqual(
+            phone_text_conversion('inbd'), '444_66_22_3')
+
+    def test_idkmybffjill(self):
+        self.assertEqual(
+            phone_text_conversion('idkmybffjill'),
+            '444_3_55_6_999_22_333_333_5_444_555_555')
+
+    def test_tisnf(self):
+        self.assertEqual(
+            phone_text_conversion('tisnf'), '666_6_4')
+
+    def test_n00b(self):
+        self.assertEqual(
+            phone_text_conversion('n00b'), '66_0_0_22')
+
+    def test_hello_world(self):
+        self.assertEqual(
+            phone_text_conversion('hello world!'),
+            '44_33_555_555_666_*_9_666_777_555_3_111')
 
 
 if __name__ == "__main__":
